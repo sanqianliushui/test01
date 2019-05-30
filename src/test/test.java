eea81772c11bb929c7a9a97cb42e05ee23f42357
sanqianliushui <51124408+sanqianliushui@users.noreply.github.com>
2019-05-30 08:55:24 +0800

    Update test.java

--- src/test/test.java
+++ src/test/test.java
@@ -4,5 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("aaa");
-		System.out.println("aba");}
+		System.out.println("aba");
+                System.out.println("666");
+    }
 }
